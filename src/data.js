e7b12a5d9350b8f00e258b4cc53d362cd1f0e7c9
ankitsrivastava0103 <ankitsrivastava0103@gmail.com>
2020-11-28 17:54:09 +0530

    Changes

--- src/data.js
+++ src/data.js
@@ -213,4 +213,4 @@ const data = [
     }
 ]
 // console.log(data)
-exports.data = data;
+exports.data = data; 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const port = 8080
+const port = 8080;
+const dataInfo = require("./data");
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-const { connection } = require('./connector')
+const { connection } = require("./connector");
 
+const dataArray = dataInfo.data;
+//console.log(dataInfo.data.length);
+app.get("/totalRecovered", (req, res) => {
+  let totalRecovered = 0;
+  for (let i = 0; i < dataArray.length; i++) {
+    totalRecovered = totalRecovered + dataArray[i].recovered;
+  }
+  res.status(200).json({ data: { _id: "total", recovered: totalRecovered } });
+});
 
-
-
-
-
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
 
 module.exports = app;
